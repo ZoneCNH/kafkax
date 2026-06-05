aa8aa2ad166fda801a62f8da521e9caf06813731
zone <zone@xhypers.local>
2026-06-05 08:20:00 +0800

    feat: sync xlib-standard template and initialize project

---
+++ examples/health/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/ZoneCNH/kafkax/pkg/kafkax"
+)
+
+func main() {
+	run(os.Stdout, os.Stderr, kafkax.Config{Name: "kafkax"})
+}
+
+func run(stdout, stderr io.Writer, cfg kafkax.Config) {
+	client, err := kafkax.New(context.Background(), cfg)
+	if err != nil {
+		_, _ = fmt.Fprintf(stderr, "create client: %v\n", err)
+		return
+	}
+
+	status := client.HealthCheck(context.Background())
+	_, _ = fmt.Fprintln(stdout, status.Status)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ZoneCNH/kafkax
+
+go 1.23
